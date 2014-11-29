9126888ede31ab5f2b60802e0cd1cbb4a238cb5f
letsleaveitempty <anita.jaszewska@gmail.com>
2014-11-29 16:56:00 +0100

    assets

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -82,3 +82,17 @@
   analytics.page();
 
 })();
+
+// accommodate Turbolinks and track page views
+$(document).on('ready page:change', function() {
+  analytics.page();
+})
+
+// accommodate Turbolinks
+// track page views and form submissions
+$(document).on('ready page:change', function() {
+  console.log('page loaded');
+  analytics.page();
+  analytics.trackForm($('#new_visitor'), 'Signed Up');
+  analytics.trackForm($('#new_contact'), 'Contact Request');
+})
